c3917d94ba2f3ea56822803e33d0aa9a00c4cc63
King Lee <lijin@chukong-inc.com>
2014-11-11 11:21:10 +0800

    add fly_flow_verify log

--- routes/fly_flow_verify.js
+++ routes/fly_flow_verify.js
@@ -2,12 +2,16 @@
  * Created by King Lee on 14-10-21.
  */
 var redis_fly_flow_wrapper = require('./nosql/redis_fly_flow_wrapper');
+var log4js = require('log4js');
+var log_json = require('../config/log.json');
+log4js.configure(log_json);
+var http_logger = log4js.getLogger('http-logger');
 
 exports.on_pay_result_verify = function(req,res){
     var orderid = req.body['orderid'];
     redis_fly_flow_wrapper.get(orderid,function(reply){
         var order_info = JSON.parse(reply);
-        console.log(order_info);
+        http_logger.debug(order_info);
         res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
         var code = "200";
         res.end(JSON.stringify({"code":code,"ret":order_info?order_info.ret:0,"amount":order_info?order_info.amount:0}));
