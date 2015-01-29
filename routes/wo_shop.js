97099c3cc91aa9a624bdabec6457b83f0268a253
King Lee <lijin@chukong-inc.com>
2015-01-29 18:25:49 +0800

    add vo_shop_verify

--- routes/wo_shop.js
+++ routes/wo_shop.js
@@ -1,33 +1,15 @@
 /**
  * Created by King Lee on 15-1-29.
  */
+var qs = require( 'querystring' );
 var log4js = require('log4js');
 var log_json = require('../config/log.json');
 log4js.configure(log_json);
 var http_logger = log4js.getLogger('http-logger');
-//var json_wo_pay = require('../config/wo_pay');
+var json_wo_pay = require('../config/wo_pay');
 
-var parsePost = function(req,res,cb){
-    var chunks = [];
-    req.on('data', function(chunk) {
-        chunks.push(chunk);
-    });
-
-    req.on('end', function() {
-        //  convert array to string,delimiter is "";
-        var data = chunks.join('');
-        //  convert url format to normal!!
-        cb(qs.parse(data));
-    });
-    req.on('error',function(err){
-        http_logger.debug('problem with request: ' + err.message);
-    });
-};
 
 exports.on_pay_result = function(req,res){
-    parsePost(req,res,function(data){
-        http_logger.debug(data);
-    });
     /*
     var serviceid = req.body['serviceid'];
     for(var v in json_wo_pay){
